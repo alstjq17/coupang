1551fbc6c7fd0cd211851b222d4185d55aa9587c
minseob <minseob15@naver.com>
2023-02-06 17:30:55 +0900

    coupang

---
+++ src/main/java/coupang/coupangman/CoupangmanApplication.java
@@ -0,0 +1,13 @@
+package coupang.coupangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupangmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coupang/coupangman/CoupangmanApplicationTests.java
@@ -0,0 +1,13 @@
+package coupang.coupangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
